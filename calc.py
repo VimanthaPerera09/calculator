5febf575cdb10af5cc7acb8b444629ae08c8834d
vimanthaperera <vimanthaperera02@gmail.com>
2024-04-15 13:49:31 +0930

    Initial push for simple calculator

---
+++ calc.py
@@ -0,0 +1,24 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        return x / y
+
+if __name__ == "__main__":
+    calc = Calculator()
+
+    print("Addition:", calc.add(10, 5))         
+    print("Subtraction:", calc.subtract(10, 5))  
+    print("Multiplication:", calc.multiply(10, 5))  
+    print("Division:", calc.divide(10, 5))       
+    print("Division:", calc.divide(10, 0))      
